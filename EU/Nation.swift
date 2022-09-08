2595e71c0376d037b59ae0b474ec3fc43207ea01
Mandy Tsang <mandy.tsang007@gmail.com>
2022-09-08 15:04:51 -0700

    Added Structs.

---
+++ EU/Nation.swift
@@ -0,0 +1,14 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by HING MAN TSANG on 9/8/22.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
